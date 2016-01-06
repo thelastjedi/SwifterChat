bc77bbe2ac807bb298ece5a47262ce92da09bff3
Harshita <thelastjedi@users.noreply.github.com>
2016-01-06 08:52:53 +0530

    add custom dates

--- SwifterChat/Models/ChatDataManager.swift
+++ SwifterChat/Models/ChatDataManager.swift
@@ -16,19 +16,20 @@ class ChatDataManager {
         let outgoingMessage: Bool
         let profilePicture : NSString
         
-        init(chatMessage : NSString, outgoingMessage: Bool) {
+        init(chatMessage : NSString, outgoingMessage: Bool, timeStamp: NSDate) {
             self.chatMessage = chatMessage
             self.outgoingMessage = outgoingMessage
-            timeStamp = NSDate()
+            self.timeStamp = timeStamp
             profilePicture = outgoingMessage ? "Cross" : "Circle"
         }
         
     }
     
-    var chatMessages = [ChatEntry(chatMessage: "A long time ago in a galaxy far, far away.", outgoingMessage: true),
-        ChatEntry(chatMessage: "It is a period of civil war. Rebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire.", outgoingMessage: false),
-        ChatEntry(chatMessage: "During the battle, Rebel spies managed to steal secret plans to the Empire's ultimate weapon, the DEATH STAR, an armored space station with enough power to destroy an entire planet.", outgoingMessage: true),
-        ChatEntry(chatMessage: "Pursued by the Empire's sinister agents, Princess Leia races home aboard her starship, custodian of the stolen plans that can save her people and restore freedom to the galaxy.", outgoingMessage: false),]
+    //pre-filling chats
+    var chatMessages = [ChatEntry(chatMessage: "A long time ago in a galaxy far, far away.", outgoingMessage: true, timeStamp: NSDate().dateByAddingTimeInterval(-3760)),
+        ChatEntry(chatMessage: "It is a period of civil war. Rebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire.", outgoingMessage: false, timeStamp: NSDate().dateByAddingTimeInterval(-3700)),
+        ChatEntry(chatMessage: "During the battle, Rebel spies managed to steal secret plans to the Empire's ultimate weapon, the DEATH STAR, an armored space station with enough power to destroy an entire planet.", outgoingMessage: true, timeStamp: NSDate().dateByAddingTimeInterval(-3640)),
+        ChatEntry(chatMessage: "Pursued by the Empire's sinister agents, Princess Leia races home aboard her starship, custodian of the stolen plans that can save her people and restore freedom to the galaxy.", outgoingMessage: false, timeStamp: NSDate().dateByAddingTimeInterval(-3600)),]
     
     
     /**
@@ -37,7 +38,7 @@ class ChatDataManager {
      - parameter outgoingMessage: boolean based on incoming/outgoing status
      */
     func addNewChat(chatMessage : NSString, outgoingMessage: Bool) {
-        chatMessages.append(ChatEntry(chatMessage: chatMessage, outgoingMessage: outgoingMessage))
+        chatMessages.append(ChatEntry(chatMessage: chatMessage, outgoingMessage: outgoingMessage, timeStamp: NSDate()))
     }
     
 }
